182c3999c8520647b644c3fbf0522a7dc43c32ed
vinhngn <vinhn.official@gmail.com>
2026-08-03 18:17:52 +0700

    feat: configure automatic local schema setup

--- src/test/java/com/clinicone/ClinicOneApplicationTests.java
+++ src/test/java/com/clinicone/ClinicOneApplicationTests.java
@@ -1,9 +1,11 @@
 package com.clinicone;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClinicOneApplicationTests {
 
 	@Test
